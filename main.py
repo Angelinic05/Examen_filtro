435a09cd9833593305da3c6ea00a82935e429ef2
Angelinic05 <152038826+Angelinic05@users.noreply.github.com>
2024-01-11 10:05:59 -0500

    filtro

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ import funciones.corefile as cf
 MenuPeliculasBlock = ('1. Administrador de generos\n2. Administador de actores\n3. Administrador de formatos\n4. Gestor de informes\n5. Gestor de peliculas\n6. salir')
 
 isActive = True
-#if __Menu__  __main__():
+if __name__ == '__main__':
 
-while(isActive):
-    os.system('cls')
-    print("")
-    print(" SISTEMA DE INFORMACION DE PELICULAS ")
-    print("-------------------------------------")
-    print(MenuPeliculasBlock)
-    try:
+    while(isActive):
+        os.system('cls')
         print("")
-        opc = int(input("opción: "))
-    except ValueError:
-        print("opción no válida...")
-        os.system("pause")
-    else:
-        if(opc == 1):
-            menu.Administradorgeneros() 
-        elif(opc == 2):
-            menu.AdministadorActores()
-        elif(opc == 3):
-            menu.AdministradorFormatos()
-        elif(opc == 4):
-            menu.GestorInformes()
-        elif(opc == 5):
-            menu.GestorPeliculas()
-        elif(opc == 6):
-            print("Hasta pronto...")
-            os.system('pause')
-            isActive = False
+        print(" SISTEMA DE INFORMACION DE PELICULAS ")
+        print("-------------------------------------")
+        print(MenuPeliculasBlock)
+        try:
+            print("")
+            opc = int(input("opción: "))
+        except ValueError:
+            print("opción no válida...")
+            os.system("pause")
+        else:
+            if(opc == 1):
+                menu.Administradorgeneros() 
+            elif(opc == 2):
+                menu.AdministadorActores()
+            elif(opc == 3):
+                menu.AdministradorFormatos()
+            elif(opc == 4):
+                menu.GestorInformes()
+            elif(opc == 5):
+                menu.GestorPeliculas()
+            elif(opc == 6):
+                print("Hasta pronto...")
+                os.system('pause')
+                isActive = False
